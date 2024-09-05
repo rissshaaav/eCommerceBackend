8fbc6ae0a526dbce742ce0ae77bac587c10af84e
rissshaaav <111266613+rissshaaav@users.noreply.github.com>
2024-09-06 01:55:54 +0530

    feat: create login route

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,10 +1,41 @@
 const User = require("../models/user.model");
+const validateFields = require("../utils/validateFields.util");
 
-const login = async (req, res)=> {
-    const {username, password} = req.body;
-    try {
-        
-    } catch (error) {
-        
+const loginController = async (req, res, next) => {
+  const { email, username, password } = req.body;
+  try {
+    const missingFields = validateFields(["password"], req.body);
+    if(missingFields){
+        const error = new Error(missingFields);
+        error.statusCode = 400;
+        throw error;
     }
-}
+    if(!username && !email){
+        const error = new Error("Either username or email must be provided");
+        error.statusCode = 400;
+        throw error;
+    }
+
+    const userFromDB = await User.findOne({
+        $or: [{email}, {username}]
+    })
+    if(!userFromDB){
+        const error = new Error("User not found");
+        error.statusCode = 404;
+        throw error;
+    }
+
+    const isMatch = await userFromDB.comparePassword(password);
+    if(!isMatch){
+        const error = new Error("Invalid Password");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    res.status(200).json({message: "Login successful", userFromDB});
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = loginController;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const signupController = require("../controllers/signup.controller")
+const signupController = require("../controllers/signup.controller");
+const loginController = require("../controllers/login.controller");
 
 userRouter.post("/signup", signupController);
+userRouter.post("/login", loginController);
 
 module.exports = userRouter;
