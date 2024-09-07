bdd3856fda476a107b1301b16cd10e11fb213649
rissshaaav <111266613+rissshaaav@users.noreply.github.com>
2024-09-07 21:21:32 +0530

    feat: create middleware to check if user is an admin.

--- middlewares/isAdmin.middleware.js
+++ middlewares/isAdmin.middleware.js
@@ -1,3 +1,33 @@
-const isAdmin = (req, res, next) => {
+const { jwtDecoded } = require("../utils/jwt.util");
+const User = require("../models/user.model");
+const validateFields = require("../utils/validateFields.util");
+
+const isAdmin = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      const error = new Error("Authentication token not found");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const decoded = jwtDecoded(token);
     
-}
+    const missingFields = validateFields(["userId", "role"], decoded);
+    if (missingFields) {
+      const error = new Error("test ", missingFields);
+      error.statusCode = 400;
+      throw error;
+    }
+
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Access Denied" });
+    }
+    const user = await User.findById(decoded.userId);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = isAdmin;
