19b866e6d45541b6c502f17850eef7180df9e87d
rissshaaav <111266613+rissshaaav@users.noreply.github.com>
2024-09-07 21:24:55 +0530

    feat: add route to update a product using its product id

---
+++ controllers/updateProduct.controller.js
@@ -0,0 +1,25 @@
+const Product = require("../models/product.model");
+
+const updateProductController = async (req, res, next) => {
+  const { productId } = req.params;
+  const { name, description, price, stock, category, imageUrl } = req.body;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { name, description, price, stock, category, imageUrl },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedProduct) {
+      const error = new Error("Product not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    res
+      .status(200)
+      .json({ message: "Product updated successfully", updatedProduct });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = updateProductController;
